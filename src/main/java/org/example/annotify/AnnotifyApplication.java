9b39330e372410b3d532bb8469599b1254663f39
TornovDutta <tornovdutta@gmail.com>
2025-11-12 09:57:07 +0530

    first commit

---
+++ src/main/java/org/example/annotify/AnnotifyApplication.java
@@ -0,0 +1,13 @@
+package org.example.annotify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnnotifyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnnotifyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/annotify/AnnotifyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.annotify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnnotifyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
